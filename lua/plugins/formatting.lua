81c86c2d95bef19f82396eac3628ef46a07f4ce9
ic768 <ic768@protonmail.com>
2024-07-21 13:38:58 +0300

    WIP

---
+++ lua/plugins/formatting.lua
@@ -0,0 +1,27 @@
+return {
+  "stevearc/conform.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  config = function()
+    local conform = require("conform")
+
+    conform.setup({
+      formatters_by_ft = {
+        javascript = { "eslint_d","prettier" },
+        typescript = { "eslint_d","prettier" },
+        javascriptreact = { "eslint_d","prettier" },
+        typescriptreact = { "eslint_d","prettier" },
+        css = { "prettier" },
+        html = { "prettier" },
+        json = { "prettier" },
+        yaml = { "prettier" },
+        markdown = { "prettier" },
+        graphql = { "prettier" },
+      },
+      format_on_save = {
+        lsp_fallback = true,
+        async = false,
+        timeout_ms = 1000,
+      },
+    })
+  end,
+}
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -17,5 +17,37 @@ return {
     config = function()
       require('render-markdown').setup({})
     end,
-  }
+  },
+  {
+    "williamboman/mason.nvim",
+    lazy=false,
+    dependencies={
+      "williamboman/mason-lspconfig.nvim",
+      "neovim/nvim-lspconfig",
+    },
+    config = function()
+      require("mason").setup()
+      require("mason-lspconfig").setup()
+
+      require("lspconfig").tsserver.setup{}
+      require("lspconfig").eslint.setup{}
+
+      vim.keymap.set("n", "g]", vim.diagnostic.goto_next)
+      vim.keymap.set("n", "g[", vim.diagnostic.goto_prev)
+
+      vim.api.nvim_create_autocmd('LspAttach', {
+        group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+
+        callback = function(ev)
+          local opts = { buffer = ev.buf }
+          vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+          vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+          vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+          vim.keymap.set({ 'n', 'v' }, '<leader>ra', vim.lsp.buf.code_action, opts)
+          vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+        end,
+      })
+
+    end
+  },
 }
