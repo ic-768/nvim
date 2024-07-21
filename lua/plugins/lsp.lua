d388becc1d5bedf77cb9ee0db8364367422d20ef
ic768 <ic768@protonmail.com>
2024-07-21 15:58:21 +0300

    Cmp works

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -46,24 +46,76 @@ return {
           vim.keymap.set({ 'n', 'v' }, '<leader>ra', vim.lsp.buf.code_action, opts)
           vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
         end,
-      })
+})
     end
+  },
+  {
+    "L3MON4D3/LuaSnip",
+    -- follow latest release.
+    version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+    -- install jsregexp (optional!).
+    build = "make install_jsregexp"
   },
   {
     "neovim/nvim-lspconfig",
     lazy = false,
+  },
+  {
+    "hrsh7th/nvim-cmp",
     dependencies = {
-      { "ms-jpq/coq_nvim", branch = "coq" },
-      { 'ms-jpq/coq.thirdparty', branch = "3p" }
+      "hrsh7th/cmp-nvim-lsp",
+      "L3MON4D3/LuaSnip",
     },
-    init = function()
-      vim.g.coq_settings = {
-        auto_start = 'shut-up',
-        ['clients.snippets.warn'] = {},
-      }
-    end,
     config = function()
-      -- Your LSP settings here
-    end,
+      local cmp = require("cmp")
+
+      vim.opt.completeopt = { "menu", "menuone", "noselect" }
+
+      cmp.setup({
+        snippet = {
+          -- REQUIRED - you must specify a snippet engine
+          expand = function(args)
+             require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ['<C-Space>'] = cmp.mapping.complete(),
+          ['<C-e>'] = cmp.mapping.abort(),
+          ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        }),
+        sources = cmp.config.sources({
+          { name = 'nvim_lsp' },
+          { name = 'luasnip' }, -- For vsnip users.
+        }, {
+          { name = 'buffer' },
+        })
+      })
+
+      -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+      cmp.setup.cmdline({ '/', '?' }, {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+          { name = 'buffer' }
+        }
+      })
+
+      -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+      cmp.setup.cmdline(':', {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = cmp.config.sources({
+          { name = 'path' }
+        }, {
+          { name = 'cmdline' }
+        }),
+        matching = { disallow_symbol_nonprefix_matching = false }
+      })
+
+      -- Set up lspconfig.
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+      require('lspconfig')['tsserver'].setup {
+        capabilities = capabilities
+      }
+
+    end
   }
 }
