ca5042b17707a2c89f65243a98c32c33817b6c45
ic768 <ic768@protonmail.com>
2024-07-21 15:30:53 +0300

    No complaints about snippets

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -54,12 +54,12 @@ return {
     lazy = false,
     dependencies = {
       { "ms-jpq/coq_nvim", branch = "coq" },
-      { "ms-jpq/coq.artifacts", branch = "artifacts" },
       { 'ms-jpq/coq.thirdparty', branch = "3p" }
     },
     init = function()
       vim.g.coq_settings = {
-        auto_start = true,
+        auto_start = 'shut-up',
+        ['clients.snippets.warn'] = {},
       }
     end,
     config = function()
