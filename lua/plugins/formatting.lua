e9d32dad748a808f2799c1a3df4c411aad30ee1d
ic-768 <ic768@protonmail.com>
2024-07-22 09:50:06 +0300

    Remove timeout

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -22,7 +22,7 @@ return {
         if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
           return
         end
-        return { timeout_ms = 500, lsp_format = "fallback" }
+        return { lsp_format = "fallback" }
       end,
     })
 
