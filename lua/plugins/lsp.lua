0f1520cedcfe2a5f91888350ba59de32ebd49dc0
ic-768 <ic768@protonmail.com>
2024-07-22 12:20:45 +0300

    Fix command mode

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -57,23 +57,11 @@ return {
         })
       })
 
-      -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
       cmp.setup.cmdline({ '/', '?' }, {
         mapping = cmp.mapping.preset.cmdline(),
         sources = {
           { name = 'buffer' }
         }
-      })
-
-      -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-      cmp.setup.cmdline(':', {
-        mapping = cmp.mapping.preset.cmdline(),
-        sources = cmp.config.sources({
-          { name = 'path' }
-        }, {
-          { name = 'cmdline' }
-        }),
-        matching = { disallow_symbol_nonprefix_matching = false }
       })
 
       -- Set up lspconfig.
