defd313a3891a74bcb7d0ee234d86fb246a24f5f
ic-768 <ic768@protonmail.com>
2024-07-22 14:29:55 +0300

    Lazydev for better lua completion

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -1,42 +1,69 @@
 return {
-	"hrsh7th/nvim-cmp",
-	dependencies = {
-		"hrsh7th/cmp-nvim-lsp",
-	},
-	config = function()
-		local cmp = require("cmp")
-
-		vim.opt.completeopt = { "menu", "menuone", "noselect" }
-
-		cmp.setup({
-			mapping = cmp.mapping.preset.insert({
-				["<C-k>"] = cmp.mapping.complete(),
-				["<C-e>"] = cmp.mapping.abort(),
-				["<C-y>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-			}),
-			sources = cmp.config.sources({
-				{ name = "nvim_lsp" },
-			}, {
-				{ name = "buffer" },
-			}),
-		})
-
-		cmp.setup.cmdline({ "/", "?" }, {
-			mapping = cmp.mapping.preset.cmdline(),
-			sources = {
-				{ name = "buffer" },
+	{
+		"folke/lazydev.nvim",
+		ft = "lua", -- only load on lua files
+		opts = {
+			library = {
+				-- See the configuration section for more details
+				-- Load luvit types when the `vim.uv` word is found
+				{ path = "luvit-meta/library", words = { "vim%.uv" } },
 			},
-		})
+		},
+	},
 
-		-- Set up lspconfig.
-		local capabilities = require("cmp_nvim_lsp").default_capabilities()
-		require("lspconfig")["tsserver"].setup({
-			capabilities = capabilities,
-		})
+	{ "Bilal2453/luvit-meta", lazy = true }, -- optional `vim.uv` typings
 
-		vim.keymap.set("n", "<C-k>", function()
-			vim.api.nvim_feedkeys("viwoeA", "n", false)
-			cmp.select_next_item()
-		end)
-	end,
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
+		},
+
+		-- for lazydev
+		opts = function(_, opts)
+			opts.sources = opts.sources or {}
+			table.insert(opts.sources, {
+				name = "lazydev",
+				group_index = 0, -- set group index to 0 to skip loading LuaLS completions
+			})
+		end,
+		--------------------
+
+		config = function()
+			local cmp = require("cmp")
+
+			vim.opt.completeopt = { "menu", "menuone", "noselect" }
+
+			cmp.setup({
+				mapping = cmp.mapping.preset.insert({
+					["<C-k>"] = cmp.mapping.complete(),
+					["<C-e>"] = cmp.mapping.abort(),
+					["<C-y>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+				}),
+				sources = cmp.config.sources({
+					{ name = "nvim_lsp" },
+				}, {
+					{ name = "buffer" },
+				}),
+			})
+
+			cmp.setup.cmdline({ "/", "?" }, {
+				mapping = cmp.mapping.preset.cmdline(),
+				sources = {
+					{ name = "buffer" },
+				},
+			})
+
+			-- Set up lspconfig.
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			require("lspconfig")["tsserver"].setup({
+				capabilities = capabilities,
+			})
+
+			vim.keymap.set("n", "<C-k>", function()
+				vim.api.nvim_feedkeys("viwoeA", "n", false)
+				cmp.select_next_item()
+			end)
+		end,
+	},
 }
