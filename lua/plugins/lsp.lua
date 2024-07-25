e2dccd9c111459c7577285e0338d4deac18c8bcc
ic-768 <ic768@protonmail.com>
2024-07-25 14:29:10 +0300

    Remove gi binding

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -37,7 +37,6 @@ return {
 				local opts = { buffer = ev.buf }
 				vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
 				vim.keymap.set("n", "gs", vim.lsp.buf.rename, opts)
-				vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
 				vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
 				vim.keymap.set({ "n", "v" }, "<leader>a", vim.lsp.buf.code_action, opts)
 				--vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
