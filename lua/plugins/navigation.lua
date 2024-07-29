ce7dfec3e04084af57e2c3aad3d4b06e6b5055c1
ic-768 <ic768@protonmail.com>
2024-07-29 12:07:47 +0300

    Able to filter through hidden files

--- lua/plugins/navigation.lua
+++ lua/plugins/navigation.lua
@@ -96,6 +96,13 @@ return {
 				},
 			})
 			require("neo-tree").setup({
+				filesystem = {
+					filtered_items = {
+						visible = true, -- This is what you want: If you set this to `true`, all "hide" just mean "dimmed out"
+						hide_dotfiles = true,
+						hide_gitignored = true,
+					},
+				},
 				mapping_options = {
 					noremap = true,
 					nowait = true,
