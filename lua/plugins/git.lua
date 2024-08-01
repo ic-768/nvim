7101f5fccef2f0f799a4be68b61dbf5d09fe60bd
ic-768 <ic768@protonmail.com>
2024-08-01 17:08:54 +0300

    Next/prev git change

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -7,7 +7,27 @@ return {
 	{
 		"lewis6991/gitsigns.nvim",
 		config = function()
-			require("gitsigns").setup()
+			require("gitsigns").setup({
+				on_attach = function(bufnr)
+					local gitsigns = require("gitsigns")
+					-- Next/prev git changes
+					vim.keymap.set("n", "g}", function()
+						if vim.wo.diff then
+							vim.cmd.normal({ "g}", bang = true })
+						else
+							gitsigns.nav_hunk("next")
+						end
+					end)
+
+					vim.keymap.set("n", "g{", function()
+						if vim.wo.diff then
+							vim.cmd.normal({ "g{", bang = true })
+						else
+							gitsigns.nav_hunk("prev")
+						end
+					end)
+				end,
+			})
 		end,
 	},
 }
