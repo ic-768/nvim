fb481bbc577fda4d04097c202f9ac20515181994
ic-768 <ic768@protonmail.com>
2024-08-12 11:25:32 +0300

    Consistent floating color

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -7,14 +7,20 @@ return {
 	config = function()
 		local colors = require("gruvbox-material.colors").get(vim.o.background, "hard")
 
+		local floatBG = "#202324"
+
 		require("gruvbox-material").setup({
 			contrast = "hard",
 			float = {
-				background_color = "#101314",
+				background_color = floatBG,
 			},
 			customize = function(g, o)
-				if o.bg == colors.bg0 then
-					o.bg = "#101314"
+				if g == "ColorColumn" then
+					o.bg = floatBG
+				else
+					if o.bg == colors.bg0 then
+						o.bg = "#101314"
+					end
 				end
 				return o
 			end,
